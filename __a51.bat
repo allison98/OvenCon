c6291e3e4fbd4e683c5b33b4bd7a19829791dc5c
DaltonWoo <daltonwoo88@gmail.com>
2018-02-11 14:58:38 -0800

    SongBeeperatStartandEnd (no 7seg)

--- __a51.bat
+++ __a51.bat
@@ -1,5 +1,5 @@
 @echo off
 C:
 cd "\Users\Dalto\Documents\GitHub\OvenCon\"
-"C:\CrossIDE\Call51\Bin\a51.exe" -l "C:\Users\Dalto\Documents\GitHub\OvenCon\oventest3.asm"
-echo Crosside_Action Set_Hex_File C:\Users\Dalto\Documents\GitHub\OvenCon\oventest3.HEX
+"C:\CrossIDE\Call51\Bin\a51.exe" -l "C:\Users\Dalto\Documents\GitHub\OvenCon\oventest4.asm"
+echo Crosside_Action Set_Hex_File C:\Users\Dalto\Documents\GitHub\OvenCon\oventest4.HEX
