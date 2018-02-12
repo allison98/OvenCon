179e476f4ef514dc12342c4bf5ec640bd0628a36
kfjustin <34084326+kfjustin@users.noreply.github.com>
2018-02-12 09:35:11 -0800

    y

--- __a51.bat
+++ __a51.bat
@@ -1,5 +1,5 @@
 @echo off
 C:
-cd "\Users\Dalto\Documents\GitHub\OvenCon\"
-"C:\CrossIDE\Call51\Bin\a51.exe" -l "C:\Users\Dalto\Documents\GitHub\OvenCon\oventest3.asm"
-echo Crosside_Action Set_Hex_File C:\Users\Dalto\Documents\GitHub\OvenCon\oventest3.HEX
+cd "\Users\Justin Li\Documents\GitHub\OvenCon\"
+"C:\CrossIDE\Call51\Bin\a51.exe" -l "C:\Users\Justin Li\Documents\GitHub\OvenCon\ovencontroller.asm"
+echo Crosside_Action Set_Hex_File C:\Users\Justin Li\Documents\GitHub\OvenCon\ovencontroller.HEX
